88acf03aa8f5a3cd5702596883e318057e87a942
emoltovi7 <59024590+emoltovi7@users.noreply.github.com>
2020-02-22 16:33:40 +0100

    new

--- js/airports.js
+++ js/airports.js
@@ -5,15 +5,40 @@ function addDatosAirports() {
 
     layerAirports  = new L.GeoJSON.AJAX(urlAirports, {
             onEachFeature: function (feature, layer) {
-                popupContent = "<b>" + feature.properties.type + "</b>"+
-                "<br>" + feature.properties.name +
-                ". " + feature.properties.continent +
-                " " + feature.properties.iso_country + "</b>";
+
+                var tipo;
+                switch(feature.properties.TIPUSCONST){
+                    case "balloonport": tipo="Aeropuerto para globo";break;
+                    case "closed":tipo="Aeropuerto cerrado";break;
+                    case "heliport":tipo="Helipuerto";break;
+                    case "large_airport":tipo="Aeropuerto grande";break;
+                    case "medium_airport":tipo="Aeropuerto mediano";break;
+                    case "seaplane_base":tipo="Aeropuerto para hidroaviones";break; 
+                    case "small_airport":tipo="Aeropuerto pequeño";break; 
+                    default: tipo="Sin descripción";
+                };
+
+                popupContent = "<b>Tipo:" + feature.properties.type + "</b>"+
+                "<br>Nombre:" + feature.properties.name +
+                ".Continente: " + feature.properties.continent +
+                " País:" + feature.properties.iso_country + "</b>";
                 layer.bindPopup(popupContent);
             },
             pointToLayer: function (feature, latlng) {
+                var fcolor;
+                switch(feature.properties.TIPUSCONST){
+                    case "balloonport":fcolor="#FF7605";break;
+                    case "closed":fcolor="#FFE805";break;
+                    case "heliport":fcolor="#007834";break;
+                    case "large_airport":fcolor="#016166";break;
+                    case "medium_airport":fcolor="#39109F";break;
+                    case "seaplane_base":fcolor="#8A0298";break; 
+                    case "small_airport":fcolor="#D7002E";break;
+                    default: fcolor="#ffffff";
+                };
+
                 return L.circleMarker(latlng, {
-                    radius: 6,
+                    radius: 3,
                     fillColor: "#00ff00",
                     color: "#ffffff",
                     weight: 3,
